a7287bcc69e302955014b2a74a2e0d07d15072b5
Jakavickis <vytautasjkvcks@gmail.com>
2022-10-04 21:19:14 +0300

    randColor

--- src/components/day1/Square.jsx
+++ src/components/day1/Square.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
-import RandNumb from "../functions/randNumb";
+import randColor from "../functions/randColor";
+import randNumb from "../functions/randNumb";
 
 function Square() {
 
     const [addSq, setAddSq] = useState([]);
 
     const addingSq = () => {
-        setAddSq(s => [...s, RandNumb(1, 100)])
+        setAddSq(s => [...s, { number: randNumb(1, 100), color: randColor() }]);
     }
+
+
 
     return (
         <>
             <button className="btn btn-green" onClick={addingSq}>Add Square</button>
             <div className="container">
                 {
-                    addSq.map((sq, i) => <div className="square" key={i}>{sq}</div>)
+                    addSq.map((sq, i) => <div style={{
+                        backgroundColor: sq.color
+                    }} className="square" key={i}>{sq.number}</div>)
                 }
             </div>
+            <div style={{
+                backgroundColor: randColor()
+            }} className="square"></div>
         </>
     )
 }
