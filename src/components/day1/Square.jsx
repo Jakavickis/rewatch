5e830b3111cc7a42f19532f01120766c5e628dc9
Jakavickis <vytautasjkvcks@gmail.com>
2022-10-04 20:55:13 +0300

    randNumb

--- src/components/day1/Square.jsx
+++ src/components/day1/Square.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import RandNumb from "../functions/randNumb";
 
 function Square() {
 
     const [addSq, setAddSq] = useState([]);
 
     const addingSq = () => {
-        setAddSq(s => [...s, ''])
+        setAddSq(s => [...s, RandNumb(1, 100)])
     }
 
     return (
         <>
+            <button className="btn btn-green" onClick={addingSq}>Add Square</button>
             <div className="container">
                 {
-                    addSq.map((_, s) => <div className="square" key={s}></div>)
+                    addSq.map((sq, i) => <div className="square" key={i}>{sq}</div>)
                 }
             </div>
-            <button className="btn btn-green" onClick={addingSq}>Add Square</button>
         </>
     )
 }
