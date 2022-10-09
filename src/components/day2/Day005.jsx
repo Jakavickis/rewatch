516f2cc761f368cb6b19a78e555712684893b99c
Jakavickis <vytautasjkvcks@gmail.com>
2022-10-09 20:34:58 +0300

    count

--- src/components/day2/Day005.jsx
+++ src/components/day2/Day005.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 function Day005() {
 
     const [fontSize, setFontZise] = useState(40);
+    const [plusingCount, setPlusingCount] = useState(0)
 
     const changeFont = () => {
         setFontZise(f => !f)
     }
+    const addingPlus = () => {
+        setPlusingCount(c => c + 1)
+    }
 
     return (
         <>
-            <h1 className="container" style={{
-                fontSize: fontSize ? '40px' : '25px'
-            }}>TEST TEST TEST</h1>
-            <button onClick={changeFont} className="btn btn-green">Font size</button>
+            <div className="container">
+                <button onClick={changeFont} className="btn btn-green">Font size</button>
+                <button onClick={addingPlus} className="btn btn-red">Plusing</button>
+            </div>
+            <div className="container">
+                <h1 className="container" style={{
+                    fontSize: fontSize ? '40px' : '25px'
+                }}>TEST TEST TEST</h1>
+                <h1>{plusingCount}</h1>
+            </div>
         </>
     )
 }
